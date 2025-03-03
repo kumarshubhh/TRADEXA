faef6e85166678d3c1f1458c1b7fc76e2ffc7763
kumarshubhh <subhanshukumar290@gmail.com>
2025-03-03 12:48:39 +0530

    Added

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function OpenAccount() {
     return ( 
         <div className="container p-5 mb-5 text-center">
-            <h1 className="mt-5">Open a Zerodha account</h1>
+            <h1 className="mt-5">Open a TRADEXA account</h1>
             <p className="mb-4">
                 Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
             </p>
